5aabc598f8afbbb9fb71f68845cc42a5a185a5e4
Nargiza Akunova <nargizaakun@gmail.com>
2022-09-18 22:09:07 +0200

    add Card wrapper component via children prop on Expenses and ExpenseItem

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import './Card.css';
 
-function Card() {
-  return <div classname="card"></div>;
+function Card(props) {
+  const classes = 'card ' + props.className;
+  return <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,29 +1,16 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2022, 2, 28);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 255.99;
-
-  // const month = props.date.toLocaleString('en-US', { month: 'long' });
-  // const year = props.date.getFullYear();
-  // const day = props.date.toLocaleString('en-US', { day: 'numeric' });
-
   return (
-    <div className="expense-item">
-      {/* <div>{props.date.toISOString()}</div> */}
-      {/* <div>
-        <div>{month} </div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div> */}
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 

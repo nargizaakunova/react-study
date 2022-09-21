e2c0298fde69d156674edf916be21c19a75059c7
Nargiza Akunova <nargizaakun@gmail.com>
2022-09-21 17:49:23 +0200

    refactor: delete button in ExpenseItem(s), make component stateless

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -9,21 +9,16 @@ const ExpenseItem = (props) => {
   //   title = 'Updated';
   //   console.log(title);
   // };
-  const [title, setTitle] = useState(props.title);
+
   console.log('ExpenseItem evaluated by React');
-  const clickHandler = () => {
-    setTitle('Updated title');
-    console.log(title);
-  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        {/* <h2>{props.title}</h2> */}
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
